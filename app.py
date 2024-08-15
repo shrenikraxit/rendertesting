db1517a5bafeb650ecb1366b22154ed0e3fc987a
shrenikraxit <129915701+shrenikraxit@users.noreply.github.com>
2024-08-15 12:34:32 -0400

    Update app.py

--- app.py
+++ app.py
@@ -6,19 +6,22 @@ socketio = SocketIO(app)
 
 messages = []
 
+
 @app.route('/')
 def index():
     return render_template('index.html')
 
+
 @socketio.on('message')
-def   
- handle_message(data):
+def handle_message(data):
     messages.append(data)
     emit('response', {'data': 'Message received from client'}, broadcast=True)
+
 
 @socketio.on('connect')
 def handle_connect():
     print('Client connected')
+
 
 if __name__ == '__main__':
     socketio.run(app)
