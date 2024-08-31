5803f3b125ee375ac1738310a2f46976eb337d7d
shrenikraxit <129915701+shrenikraxit@users.noreply.github.com>
2024-08-31 13:28:03 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request
 from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
 
+@app.route('/upload', methods=['POST'])
+def upload_file():
+  if 'file' not in request.files:
+    return 'No file part'
 
+  file = request.files['file']
+  file.save('uploaded_file.jpeg')  # Adjust the file name and path as needed
+
+  return 'File uploaded successfully'
+
+'''
 @app.route('/export-images', methods=['POST'])
 def export_images():
     data = request.get_json()
@@ -17,6 +27,14 @@ def export_images():
         return jsonify({"message": "Images received and processed successfully!"}), 200
     else:
         return jsonify({"message": "No images received!"}), 400
+
+
+'''
+
+
+
+
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
