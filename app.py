5529d517d1a2f690683552edb643938493fb2034
shrenikraxit <129915701+shrenikraxit@users.noreply.github.com>
2024-08-31 13:30:22 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
+# Python (Flask)
 from flask import Flask, request
-from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app)
 
 @app.route('/upload', methods=['POST'])
 def upload_file():
@@ -14,27 +13,5 @@ def upload_file():
 
   return 'File uploaded successfully'
 
-'''
-@app.route('/export-images', methods=['POST'])
-def export_images():
-    data = request.get_json()
-    images = data.get('images', [])
-
-    if images:
-        # Here, you can save the images or process them as needed
-        # For example, save them to the filesystem or a database
-
-        return jsonify({"message": "Images received and processed successfully!"}), 200
-    else:
-        return jsonify({"message": "No images received!"}), 400
-
-
-'''
-
-
-
-
-
-
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+  app.run(debug=True)
